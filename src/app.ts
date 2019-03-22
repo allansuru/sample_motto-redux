43151f41da56bd9e999cdb248084f4c8e6f0ed46
Allan SuRu <https://github.com/allansuru/ProjetoBase.git>
2019-03-21 21:48:30 -0300

    All about Reducers

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,48 @@
-import * as fromStore from './store';
+import * as fromStore from "./store";
 
-import { renderTodos } from './utils';
+import { renderTodos } from "./utils";
 
-const input = document.querySelector('input') as HTMLInputElement;
-const button = document.querySelector('button') as HTMLButtonElement;
-const destroy = document.querySelector('.unsubscribe') as HTMLButtonElement;
-const todoList = document.querySelector('.todos') as HTMLLIElement;
+const input = document.querySelector("input") as HTMLInputElement;
+const button = document.querySelector("button") as HTMLButtonElement;
+const destroy = document.querySelector(".unsubscribe") as HTMLButtonElement;
+const todoList = document.querySelector(".todos") as HTMLLIElement;
 
-const store = new fromStore.Store(
-  {},
-  {
-    todos: [{ label: "Eat pizza", complete: false }]
-  }
-);
+const reducer = {
+  todos: fromStore.reducer
+};
+
+// const state = {
+//   todos: fromStore.initialState
+// };
+
+const store = new fromStore.Store(reducer, {});
 
 // console.log(store.value)
 
-button.addEventListener('click', () => {
+button.addEventListener(
+  "click",
+  () => {
     if (!input.value.trim()) return;
 
     const payload = { label: input.value, complete: false };
 
-  // console.log(payload);
+    // console.log(payload);
 
-  store.dispatch({
-    type: 'ADD_TODO',
-    payload
-  });
+    store.dispatch({
+      type: "ADD_TODO",
+      payload
+    });
 
-    input.value = '';
+    console.log(store.value);
+
+    input.value = "";
   },
   false
 );
 
-todoList.addEventListener('click', function(event) {
+todoList.addEventListener("click", function(event) {
   const target = event.target as HTMLButtonElement;
-  if (target.nodeName.toLowerCase() === 'button') {
+  if (target.nodeName.toLowerCase() === "button") {
     console.log(target);
   }
 });
